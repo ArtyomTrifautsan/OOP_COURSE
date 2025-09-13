4281979514f22023b7fc253219204b7fcdbdb9d4
ArtyomTrifautsan <trifautsan01@mail.ru>
2025-09-13 21:19:07 +0700

    Make serialize function more universal and add sequential containers supporting.
    I made a single serialize function that is called for all data types. There
    were 2 serialize function templates in the previous commit. But I decided that
    more correctly is making serilizer and deserializer templates, that are called
    by the single serialize function
    I developed serializer and deserializer for sequential containers (like
    std::vector) using concepts, and made test calls in example_of_usage/main.cpp.
    I haven't written any tests yet.
    I developed serializer and deserializer for std::pair, but I didn't test those
    structures.

--- task-1/example_of_use/src/main.cpp
+++ task-1/example_of_use/src/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <iomanip>
 #include <vector>
+#include <list>
 
 #include <serialize.hpp>
 
@@ -20,25 +21,25 @@ void print_bytes(const T& obj)
     std::cout << std::dec << std::endl;
 }
 
-template <>
-void print_bytes(const std::string& str)
-{
-    const uint32_t len = static_cast<uint32_t>(str.size());
-    const uint8_t* len_ptr = reinterpret_cast<const uint8_t*>(&len);
-    std::cout << "Bytes (size: " << sizeof(len) + len << "): ";
-    for (size_t i = 0; i < sizeof(len); ++i) {
-        std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
-                  << static_cast<int>(len_ptr[i]) << " ";
-    }
+// template <>
+// void print_bytes(const std::string& str)
+// {
+//     const uint32_t len = static_cast<uint32_t>(str.size());
+//     const uint8_t* len_ptr = reinterpret_cast<const uint8_t*>(&len);
+//     std::cout << "Bytes (size: " << sizeof(len) + len << "): ";
+//     for (size_t i = 0; i < sizeof(len); ++i) {
+//         std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
+//                   << static_cast<int>(len_ptr[i]) << " ";
+//     }
 
-    const uint8_t* data_ptr = reinterpret_cast<const uint8_t*>(str.data());
-    for (size_t i = 0; i < len; ++i) {
-        std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
-                  << static_cast<int>(data_ptr[i]) << " ";
-    }
+//     const uint8_t* data_ptr = reinterpret_cast<const uint8_t*>(str.data());
+//     for (size_t i = 0; i < len; ++i) {
+//         std::cout << std::hex << std::uppercase << std::setw(2) << std::setfill('0')
+//                   << static_cast<int>(data_ptr[i]) << " ";
+//     }
 
-    std::cout << std::dec << std::endl;
-}
+//     std::cout << std::dec << std::endl;
+// }
 
 
 int main(int argc, char const *argv[])
@@ -49,11 +50,18 @@ int main(int argc, char const *argv[])
     std::string hello_str = "hello";
     std::vector<int> numbers = {1, 2, 3, 5, 4};
 
+    std::cout << "===For char[7]: ";
     print_bytes(hello);
+    std::cout << "===For std::string: ";
     print_bytes(hello_str);
+    std::cout << "===For std::vector: ";
+    print_bytes(numbers);
 
+    // std::cout << "===For char[7]: ";
     serialize(hello, ofs);
+    // std::cout << "===For std::string: ";
     serialize(hello_str, ofs);
+    // std::cout << "===For std::vector: ";
     serialize(numbers, ofs);
 
     ofs.close();
@@ -69,18 +77,30 @@ int main(int argc, char const *argv[])
     deserialize(hello_str_2, ifs);
     deserialize(numbers2, ifs);
     
+    std::cout << "===For char[7]: ";
     print_bytes(hello2);
+    std::cout << "===For std::string: ";
     print_bytes(hello_str_2);
-
-    std::cout << hello_str_2 << std::endl;
+    std::cout << "===For std::vector: ";
+    print_bytes(numbers2);
 
     ifs.close();
 
-    if (hello_str == hello_str_2) std::cout << "strings equal!!!" << std::endl;
+    // if (hello_str == hello_str_2) std::cout << "strings equal!!!" << std::endl;
+
+    std::cout << "hello2: ";
+    for (auto item : hello2) std::cout << item;
+    std::cout << std::endl;
+
+    std::cout << "hello_str_2: " << hello_str_2 << std::endl;
 
     std::cout << "Numbers2: ";
     for (auto item : numbers2) std::cout << item;
-    std::cout <<std::endl;
+    std::cout << std::endl;
+
+    // std::cout << "List Numbers2: ";
+    // for (auto item : list_numbers2) std::cout << item;
+    // std::cout << std::endl;
 
     return 0;
 }
--- task-1/serialize_deserialize/includes/serialize.hpp
+++ task-1/serialize_deserialize/includes/serialize.hpp
@@ -4,14 +4,53 @@
 #include <iterator>
 #include <algorithm>
 #include <cstdint>
-#include <string>
+#include <type_traits>
+#include <concepts>
+
 #include <vector>
+#include <string>
+#include <array>
+// #include
 
 
-/*
-Точно добавить SFINAI
-Возможно добавить концепты
-*/
+
+template<typename T>
+concept String = std::is_same_v<T, std::string>;
+
+
+template <typename ContainerType>
+concept Container = !String<ContainerType> && requires(ContainerType& a)
+{
+    typename ContainerType::value_type;
+    typename ContainerType::reference;
+    typename ContainerType::const_reference;
+    typename ContainerType::iterator;
+    typename ContainerType::const_iterator;
+    typename ContainerType::size_type;
+
+    { a.begin() } -> std::same_as<typename ContainerType::iterator>;
+    { a.end() } -> std::same_as<typename ContainerType::iterator>;
+    { a.cbegin() } -> std::same_as<typename ContainerType::const_iterator>;
+    { a.cend() } -> std::same_as<typename ContainerType::const_iterator>;
+    { a.size() } -> std::same_as<typename ContainerType::size_type>;
+
+    { a.clear() };
+};
+
+template <typename ContainerType>
+concept SequentialContainer = Container<ContainerType> && requires(ContainerType c, typename ContainerType::value_type v) 
+{
+    c.push_back(v);
+};
+
+template <typename ContainerType>
+concept AssociativeContainer = Container<ContainerType> && requires()
+{
+    typename ContainerType::key_type;
+};
+
+
+
 
 
 // ===DECLARATIONS===
@@ -23,11 +62,19 @@ template <typename T>
 void deserialize(T& obj, std::istream& is);
 
 
-template <typename T>
-void serialize(const std::vector<T>& vector, std::ostream& os);
+// template <typename T>
+// void serialize(const std::vector<T>& vector, std::ostream& os);
 
-template <typename T>
-void deserialize(std::vector<T>& vector, std::istream& is);
+// template <typename T>
+// void deserialize(std::vector<T>& vector, std::istream& is);
+
+// template <typename ContainerType>
+// requires SequentialContainer<ContainerType>
+// void serialize(const ContainerType& c, std::ostream& os);
+
+// template <typename ContainerType>
+// requires SequentialContainer<ContainerType>
+// void deserialize(ContainerType& c, std::istream& is);
 
 
 // ===IMPLEMENTATIOS===
@@ -37,7 +84,7 @@ struct serializer
 {
     static void apply(const T& obj, std::ostream& os)
     {
-        std::cout << "Using base serializer" << std::endl;
+        // std::cout << "Using base serializer" << std::endl;
 
         const uint8_t* ptr = reinterpret_cast<const uint8_t *>(&obj);
 
@@ -52,7 +99,7 @@ struct deserializer
 {
     static void apply(T& val, std::istream& is)
     {
-        std::cout << "Using base deserializer" << std::endl;
+        // std::cout << "Using base deserializer" << std::endl;
 
         uint8_t* ptr = reinterpret_cast<uint8_t*>(&val);
 
@@ -73,7 +120,7 @@ struct serializer<std::string>
 {
     static void apply(const std::string& str, std::ostream& os)
     {
-        std::cout << "Using string serializer" << std::endl;
+        // std::cout << "Using string serializer" << std::endl;
 
         // Write the len of the string
         const uint32_t len = static_cast<uint32_t>(str.size());
@@ -91,12 +138,12 @@ struct deserializer<std::string>
 {
     static void apply(std::string& str, std::istream& is)
     {
-        std::cout << "Using string deserializer" << std::endl;
+        // std::cout << "Using string deserializer" << std::endl;
 
         // Read the len of the string
         uint32_t len = 0;
         deserialize(len, is);
-        std::cout << "len == " << len << std::endl;
+        // std::cout << "len == " << len << std::endl;
 
         // Read the characters
         str.clear();
@@ -107,6 +154,74 @@ struct deserializer<std::string>
 };
 
 
+template <typename T1, typename T2>
+struct serializer<std::pair<T1, T2>>
+{
+    static void apply(const std::pair<T1, T2>& pair, std::ostream& os)
+    {
+        // std::cout << "Using pair serializer" << std::endl;
+
+        serialize(pair.first, os);
+        serialize(pair.second, os);
+    }
+};
+
+template <typename T1, typename T2>
+struct deserializer<std::pair<T1, T2>>
+{
+    static void apply(std::pair<T1, T2>& pair, std::istream& is)
+    {
+        // std::cout << "Using pair deserializer" << std::endl;
+
+        deserialize(pair.first, is);
+        deserialize(pair.second, is);
+    }
+};
+
+
+// ===Sequential containers===
+template <SequentialContainer ContainerType>
+struct serializer<ContainerType>
+{
+    static void apply(const ContainerType& c, std::ostream& os)
+    {
+        // std::cout << "Using serializer for sequential container" << std::endl;
+
+        // Write the len of the string
+        const uint32_t len = static_cast<uint32_t>(c.size());
+        serialize(len, os);
+
+        // Write items of vector
+        for (const typename ContainerType::value_type& obj : c)
+            serialize(obj, os);
+    }
+};
+
+template <SequentialContainer ContainerType>
+struct deserializer<ContainerType>
+{
+    static void apply(ContainerType& c, std::istream& is)
+    {
+        // std::cout << "Using deserializer for sequential container" << std::endl;
+
+        c.clear();
+
+        // Read the len of array
+        uint32_t len = 0;
+        deserialize(len, is);
+
+        // Read objects
+        for (uint32_t i = 0; i < len; i++)
+        {
+            typename ContainerType::value_type obj{};
+            deserialize(obj, is);
+            c.push_back(obj);
+        }
+    }
+};
+
+
+/*
 //===Supporting std::vector
 template <typename T>
 struct vector_serializer
@@ -148,6 +263,7 @@ struct vector_deserializer
         }
     }
 };
+*/
 
 
 template <typename T>
@@ -163,14 +279,29 @@ void deserialize(T& obj, std::istream& is)
 }
 
 
-template <typename T>
-void serialize(const std::vector<T>& vector, std::ostream& os)
-{
-    vector_serializer<T>::apply(vector, os);
-}
+// template <typename T>
+// void serialize(const std::vector<T>& vector, std::ostream& os)
+// {
+//     vector_serializer<T>::apply(vector, os);
+// }
 
-template <typename T>
-void deserialize(std::vector<T>& vector, std::istream& is)
-{
-    vector_deserializer<T>::apply(vector, is);
-}
+// template <typename T>
+// void deserialize(std::vector<T>& vector, std::istream& is)
+// {
+//     vector_deserializer<T>::apply(vector, is);
+// }
+
+
+// template <typename ContainerType>
+// requires SequentialContainer<ContainerType>
+// void serialize(const ContainerType& c, std::ostream& os)
+// {
+//     sequential_container_serializer<ContainerType>::apply(c, os);
+// }
+
+// template <typename ContainerType>
+// requires SequentialContainer<ContainerType>
+// void deserialize(ContainerType& c, std::istream& is)
+// {
+//     sequential_container_deserializer<ContainerType>::apply(c, is);
+// }
