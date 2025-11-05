e3a7401e1ad96ffb0d75b845ae340f4586c61f00
ArtyomTrifautsan <trifautsan01@mail.ru>
2025-11-05 09:48:46 +0700

    Finish realisation of SharedPtr and write tests

--- task-4/src/shared_pointer.hpp
+++ task-4/src/shared_pointer.hpp
@@ -1,207 +1,250 @@
 #pragma once
 
+#include <utility>
+#include <iostream>
+#include <type_traits>
 
-namespace SharedPointer {
 
-    template<class Type, class TDeleter>
-    class Pointer {
-        using t_Pointer = Pointer<Type, TDeleter>;
+namespace Pointers {
+
+    namespace _Impl {
+
+        template<typename Type>
+        class BaseDeleter
+        {
+        public:
+            void operator()(Type* ptr) const noexcept { delete ptr; }
+        };
+
+
+        template <typename Type, typename TDeleter = BaseDeleter<Type>>
+        class ControlBlock
+        {
+        public:
+
+            ControlBlock(Type* _obg, TDeleter _deleter = {}) : m_deleter{_deleter}, m_shared_ref_counter{1}
+            {
+                m_obj_ptr = _obg;
+            }
+
+            template <typename... Args>
+            ControlBlock(Args&&... args) : m_shared_ref_counter{1}
+            {
+                m_obj_ptr = new Type(std::forward<Args>(args)...);
+            }
+
+            ~ControlBlock()
+            {
+            }
+
+            void add_shared_ref() noexcept { ++m_shared_ref_counter; }
+
+            void remove_shared_ref() noexcept 
+            {
+                --m_shared_ref_counter; 
+
+                if (m_shared_ref_counter == 0)
+                    m_deleter(m_obj_ptr);
+
+                if (m_shared_ref_counter == 0 && m_weak_ref_counter == 0)
+                    delete this;
+            }
+
+            void add_weak_ref() noexcept { ++m_weak_ref_counter; }
+
+            void remove_weak_ref() noexcept 
+            {
+                --m_weak_ref_counter;
+
+                if (m_shared_ref_counter == 0 && m_weak_ref_counter == 0)
+                    delete this;
+            }
+
+            Type* get_obj_ptr() noexcept { return m_obj_ptr; }
+
+            int count_shared_refs() const noexcept { return m_shared_ref_counter; }
+
+        private:
+            Type* m_obj_ptr = nullptr;
+            TDeleter m_deleter;
+            int m_shared_ref_counter = 0;       // Или size_t?
+            int m_weak_ref_counter = 0;         // Или size_t?
+        };
+
+
+        
+
+    };
+
+    template<typename Type, typename TDeleter = _Impl::BaseDeleter<Type>>
+    class SharedPTR {
+        using t_SharedPTR = SharedPTR<Type, TDeleter>;
 
     public: // Constructors and destructor.
-        Pointer() noexcept = default;
+        SharedPTR() noexcept = default;
 
-        Pointer(Type *pObj, TDeleter deleter_ = {}) noexcept
+        SharedPTR(Type *pObj, TDeleter deleter_ = {}) noexcept
         {
-            // А что будет если передать сюда стековый указатель? У нас же не сработает delete expression
             if (pObj == nullptr)
-            {
                 return;
-            }
-            
-            {
-                m_obj_ptr = pObj;
-                *m_ref_counter = 1;
-            }
 
-            m_deleter = deleter_;
+            m_control_block = new _Impl::ControlBlock(pObj, deleter_);
         }
 
-        Pointer(Pointer&& uniquePTR) noexcept // Move constructor.
+        SharedPTR(t_SharedPTR&& other) noexcept
         {
-    
+            if (other.m_control_block == nullptr) return;
 
-            m_obj_ptr = uniquePTR.m_obj_ptr;
-            m_ref_counter = uniquePTR.m_ref_counter;
-            m_deleter = uniquePTR.m_deleter;
-
-            ++(*m_ref_counter);
-
-            uniquePTR.m_ref_counter = nullptr;
+            std::swap(m_control_block, other.m_control_block);
         }
 
-        Pointer(const t_Pointer& other)
+        SharedPTR(const t_SharedPTR& other)
         {
-            // Мы же не можем вызвать конструктор у уже существующего объекта?
+            if (other.m_control_block == nullptr) return;
 
-
-            m_obj_ptr = other.m_obj_ptr;
-            m_ref_counter = other.m_ref_counter;
-            m_deleter = other.m_deleter;
-
-            ++(*m_ref_counter);
+            m_control_block = other.m_control_block;
+            m_control_block->add_shared_ref();
         }
 
-        ~Pointer()
+        ~SharedPTR()
         {
-            if (*m_ref_counter == 0){}
+            if (m_control_block == nullptr) return;
 
-            else if (*m_ref_counter == 1)
-            {
-                if (m_obj_ptr != nullptr)
-                {
-                    m_deleter(m_obj_ptr);
-                    m_obj_ptr = nullptr;
-                }
-                *m_ref_counter = 0;
-            }
-
-            else
-            {
-                --(*m_ref_counter);
-            }
+            m_control_block->remove_shared_ref();
         }
 
     public: // Assignment.
-        t_Pointer& operator=(t_Pointer &&Pointer) noexcept
+        t_SharedPTR& operator=(t_SharedPTR&& other) noexcept
         {
             if (other == *this) return *this;
 
-            // m_obj_ptr = uniquePTR.m_obj_ptr;
-            std::swap(m_obj_ptr, uniquePTR.m_obj_ptr);
-            // m_ref_counter = uniquePTR.m_ref_counter;
-            std::swap(m_ref_counter, uniquePTR.m_ref_counter);  // делаю чтобы гарантировать noexcept
-            // m_deleter = uniquePTR.m_deleter;
-            std::swap(m_deleter, uniquePTR.m_deleter);
+            // release()    // Сомневаюсь что тут надо делать release()
 
-            ++(*m_ref_counter);
+            std::swap(m_control_block, other.m_control_block);
 
-            return *this
+            return *this;
         }
 
-        t_Pointer& operator=(Type *pObject)
+        t_SharedPTR& operator=(Type *pObj)
         {
+            if (m_control_block != nullptr && pObj == m_control_block->get_obj_ptr())
+                return *this;
+
             release();
 
-            if (pObj == nullptr)
-            {
-                m_obj_ptr = nullptr;
-                *m_ref_counter = 0;
-            }
-            else 
-            {
-                m_obj_ptr = pObj;
-                *m_ref_counter = 1;
-            }
+            if (pObj != nullptr) 
+                m_control_block = new _Impl::ControlBlock(pObj);
 
-            m_deleter{};
+            return *this;
         }
 
-        t_Pointer& operator=(const t_Pointer&)
+        t_SharedPTR& operator=(const t_SharedPTR& other)
         {
             if (other == *this) return *this;
 
             release();
 
-            m_obj_ptr = other.m_obj_ptr;
-            m_ref_counter = other.m_ref_counter;
-            m_deleter = other.m_deleter;
-
-            ++(*m_ref_counter);
+            if (other.m_control_block != nullptr)
+            {
+                m_control_block = other.m_control_block;
+                m_control_block->add_shared_ref();
+            }
 
             return *this;
         }
 
     public: // Observers.
     // Dereference the stored pointer.
-        Type& operator*() const { return *m_obj_ptr; }
+        Type& operator*() const { return *(m_control_block->get_obj_ptr()); }
 
         // Return the stored pointer.
-        Type* operator->() const { return m_obj_ptr; }
+        Type* operator->() const { return m_control_block->get_obj_ptr(); }
 
         // Return the stored pointer.
-        Type* get() const { return m_obj_ptr; }
-
-        // Return a reference to the stored deleter.
-        TDeleter& get_deleter() { return m_deleter; }
+        Type* get() const
+        {
+            return m_control_block != nullptr ? m_control_block->get_obj_ptr() : nullptr;
+        }
 
         // Return false if the stored pointer is null.
-        operator bool() const { return m_obj_ptr != nullptr; }
+        operator bool() const { return m_control_block != nullptr; }
+
+        int count_refs() const noexcept
+        {
+            if (m_control_block == nullptr) return 0;
+
+            return m_control_block->count_shared_refs();
+        }
 
     public: // Modifiers.
         // Release ownership of any stored pointer.
-        void release() 
+        void release()
         {
-            if (m_obj_ptr == nullptr) return;
+            if (m_control_block == nullptr) return;
 
-            if (*m_ref_counter > 1)
-            {
-                m_obj_ptr = nullptr;
-                --(*m_ref_counter);
-            }
-            else
-            {
-                m_deleter(m_obj_ptr);
-                m_obj_ptr = nullptr;
-                *m_ref_counter = 0;
-            }
+            m_control_block->remove_shared_ref();
+            m_control_block = nullptr;
         }
 
         // Replace the stored pointer.
-        void reset(Type *pObject = nullptr)
+        void reset(Type *pObj = nullptr)
         {
-            /*
-            m_obj_ptr и pObject -- nullptr
-            ничего не происходит, по сути просто сбрасываем и так пустой указатель
+            release();
 
-            m_obj_ptr -- nullptr, pObject - нет
-            по сути создаем первый указатель на pObject, т.е. по аналогии с конструктором
+            if (pObj == nullptr) return;
 
-            m_obj_ptr -- нет, pObject - nullptr
-            сбрасываем существующий укзатель, делая его пустым
-
-            m_obj_ptr и pObject - не nullptr
-            по сути как оператор присваивания
-            */
-
-            // if (m_obj_ptr == nullptr)
-            // {
-            //     m_obj_ptr = pObj;
-            //     *m_ref_counter = 1;
-            //     m_deleter{};
-            // }
-            // else
-            // {
-            //     if (*m_ref_counter > 1)
-            //     {
-            //         --(*m_ref_counter);
-            //         m_obj_ptr = pObject;
-            //     }
-            //     else
-            //     {
-            //         m_deleter(m_obj_ptr);
-            //         m_obj_ptr = nullptr;
-            //         *m_ref_counter = 0;
-            //     }
-            // }
+            m_control_block = new _Impl::ControlBlock(pObj);
         }
 
         // Exchange the pointer with another object.
-        void swap(t_Pointer &Pointer); 
+        void swap(t_SharedPTR& other)
+        {
+            if (*this == other) return;
+
+            std::swap(m_control_block, other.m_control_block);
+        }
+
+        friend bool operator==(const t_SharedPTR& first, const t_SharedPTR& second)
+        {
+            return first.m_control_block == second.m_control_block;
+        }
+
 
     private:
-        Type* m_obj_ptr = nullptr;
-        int* m_ref_counter = 0;
-        TDeleter m_deleter{};
+        _Impl::ControlBlock<Type, TDeleter>* m_control_block = nullptr;
     };
+
+
+    template<typename Type>
+    SharedPTR<Type, _Impl::BaseDeleter<Type>> make_shared()
+    {
+        return SharedPTR<Type, _Impl::BaseDeleter<Type>>();
+    }
+
+
+    template<typename Type, typename... Args>
+    requires (!std::is_same_v<Args, SharedPTR<Type, _Impl::BaseDeleter<Type>>&> && ...)
+    SharedPTR<Type, _Impl::BaseDeleter<Type>> make_shared(Args&&... args)
+    // auto make_shared(Args&&... args) -> std::enable_if_t<(sizeof...(Args) > 0) && !std::is_same_v<std::decay>>
+    {
+        /*
+        На cpp reference написано, что при использовании std::make_shared происходит только одна
+        аллокация памяти, но я не придумал как правильно реализовать эту затею.
+
+        У меня только одна идея. Сделать класс ControlBlock интерфейсом, и сделать две реализации 
+        интерфейса: MonolitControlBlock и NonMonolitControlBlock. Первый класс будет использоваться
+        при создании SharedPTR с помощью функций make_shared, а второй при создании SharedPTR с 
+        помощью конструкторов.
+        */
+        Type* pObj = new Type(std::forward<Args>(args)...);
+        return SharedPTR<Type, _Impl::BaseDeleter<Type>>(pObj);
+    }
+
+
+    template<typename Type>
+    SharedPTR<Type, _Impl::BaseDeleter<Type>> make_shared(const SharedPTR<Type, _Impl::BaseDeleter<Type>>& other)
+    {
+        return SharedPTR(other);
+    }
 
 }
--- task-4/src/test_shared_pointer.cpp
+++ task-4/src/test_shared_pointer.cpp
@@ -1,9 +1,593 @@
 #include <gtest/gtest.h>
 
 #include <iostream>
+#include <vector>
+
+#include "shared_pointer.hpp"
 
 
-TEST(TestSharedPtr, First)
+// Этот класс нужен для тестов, в которых я проверяю, что объект уничтожился
+class TestClass
 {
-    std::cout << "Hello from tests." << std::endl;
-}   
+public:
+    TestClass() = default; 
+    TestClass(bool* _flag) : m_delete_flag_ptr{_flag} {}
+    ~TestClass()
+    {
+        *m_delete_flag_ptr = true;
+        m_delete_flag_ptr = nullptr;
+    }
+private:
+    bool* m_delete_flag_ptr = nullptr;
+};
+
+
+// Этот класс нужен для теста, в котором я проверяю, что объект конструируется только один раз,
+// несмотря на создание нескольких shared_ptr
+class TestClassWithCounter
+{
+public:
+    TestClassWithCounter() = default; 
+    TestClassWithCounter(int* _counter) : m_counter_ptr{_counter} { *m_counter_ptr += 1; }
+    ~TestClassWithCounter() {}
+private:
+    int* m_counter_ptr = nullptr;
+};
+
+
+// Этот класс нужен для тестирования пользовательского deleter
+class CustomDeleter
+{
+public:
+    CustomDeleter() = default;
+    CustomDeleter(bool* flag) : m_deleted{flag} {}
+    void operator()(int* ptr)
+    {
+        delete ptr;
+        *m_deleted = true;
+    }
+private:
+    bool* m_deleted = nullptr;
+};
+
+
+// Класс для тестирвоания make_shared функции с перменным количеством параметров
+class Point
+{
+public:
+    Point(int _x, int _y) : m_x{_x}, m_y{_y} {}
+    int x() const noexcept { return m_x; }
+    int y() const noexcept { return m_y; }
+private:
+    int m_x = 0;
+    int m_y = 0;
+};
+
+
+
+TEST(DefaultConstructor, DefaultConstructor)
+{
+    Pointers::SharedPTR<int> p{};
+
+    EXPECT_EQ(p, nullptr);
+    EXPECT_EQ(p.get(), nullptr);
+    EXPECT_EQ(p.count_refs(), 0);
+}
+
+
+// TEST(Constructor, EmptyIntArray)
+// {
+//     class SpecialDeleter
+//     {
+//     public:
+//         void operator()(int* ptr) { delete[] ptr; }  
+//     };
+//     Pointers::SharedPTR<int[5], SpecialDeleter> p{};
+
+//     EXPECT_EQ(p, nullptr);
+//     EXPECT_EQ(p.get(), nullptr);
+// }
+
+
+TEST(ConstructorWithRawPointer, Nullptr)
+{
+    Pointers::SharedPTR<int> p{nullptr};
+
+    EXPECT_EQ(p.get(), nullptr);
+    EXPECT_EQ(p.count_refs(), 0);
+}
+
+
+TEST(ConstructorWithRawPointer, InitializedPointer)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+}
+
+
+TEST(MoveConstructor, EmptyPointer)
+{
+    Pointers::SharedPTR<int> p{};
+
+    Pointers::SharedPTR p2 = std::move(p);
+
+    EXPECT_EQ(p2.get(), nullptr);
+    EXPECT_EQ(p2.count_refs(), 0);
+}
+
+
+TEST(MoveConstructor, PointerWithValue)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+
+    Pointers::SharedPTR p2 = std::move(p);
+
+    EXPECT_EQ(p2.get(), raw_p);
+    EXPECT_EQ(p2.count_refs(), 1);
+}
+
+
+TEST(CopyConstructor, EmptyPointer)
+{
+    Pointers::SharedPTR<int> p{};
+    EXPECT_EQ(p.get(), nullptr);
+
+    Pointers::SharedPTR p2 = p;
+
+    EXPECT_EQ(p2.get(), nullptr);
+    EXPECT_EQ(p2.count_refs(), 0);
+}
+
+
+TEST(CopyConstructor, PointerWithValue)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+
+    Pointers::SharedPTR p2 = p;
+
+    EXPECT_EQ(p.get(), p2.get());
+    EXPECT_EQ(p.count_refs(), 2);
+}
+
+
+TEST(AssigmentRawPointer, Nullptr)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    int* another_raw_p = new int(10);
+    p = another_raw_p;
+    EXPECT_EQ(p.get(), another_raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+}
+
+
+TEST(AssigmentRawPointer, InitializedPtr)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    int* another_raw_p = new int(10);
+    p = another_raw_p;
+    EXPECT_EQ(p.get(), another_raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+}
+
+
+TEST(AssigmentRawPointer, CheckDeletingMemory)
+{
+    bool delete_flag = false;
+    TestClass* raw_p = new TestClass(&delete_flag);
+    Pointers::SharedPTR p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    bool another_delete_flag = false;
+    TestClass* another_raw_p = new TestClass(&another_delete_flag);
+    p = another_raw_p;
+
+    EXPECT_EQ(delete_flag, true);
+    EXPECT_EQ(another_delete_flag, false);
+
+    EXPECT_EQ(p.get(), another_raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+}
+
+
+TEST(AssigmentRawPointer, OwnRawPointer)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    p = raw_p;
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+}
+
+
+TEST(MoveAssigment, EmptyPointer)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    int* another_raw_p = new int(10);
+    Pointers::SharedPTR<int> p2{another_raw_p};
+    EXPECT_EQ(p2.get(), another_raw_p);
+
+    p2 = std::move(p);
+
+    EXPECT_EQ(p2.get(), raw_p);
+    EXPECT_EQ(p2.count_refs(), 1);
+}
+
+
+TEST(MoveAssigment, PointerWithValue)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    int* another_raw_p = new int(10);
+    Pointers::SharedPTR<int> p2{another_raw_p};
+    EXPECT_EQ(p2.get(), another_raw_p);
+
+    p2 = std::move(p);
+
+    EXPECT_EQ(p2.get(), raw_p);
+    EXPECT_EQ(p2.count_refs(), 1);
+}
+
+
+TEST(MoveAssigment, CheckDeletingMemory)
+{
+    bool delete_flag = false;
+    TestClass* raw_p = new TestClass(&delete_flag);
+    Pointers::SharedPTR p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    bool another_delete_flag = false;
+    TestClass* another_raw_p = new TestClass(&another_delete_flag);
+    Pointers::SharedPTR another_p{another_raw_p};
+    EXPECT_EQ(another_p.get(), another_raw_p);
+
+    another_p = std::move(p);
+
+    EXPECT_EQ(delete_flag, false);
+    EXPECT_EQ(another_delete_flag, false);
+}
+
+
+TEST(MoveAssigment, SelfMove)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    p = std::move(p);
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+}
+
+
+TEST(CopyAssigment, EmptyPointer)
+{
+    Pointers::SharedPTR<int> p{};
+    EXPECT_EQ(p.get(), nullptr);
+
+    int* another_raw_p = new int(10);
+    Pointers::SharedPTR<int> p2{another_raw_p};
+    EXPECT_EQ(p2.get(), another_raw_p);
+
+    p2 = p;
+
+    EXPECT_EQ(p2.get(), nullptr);
+    EXPECT_EQ(p2.count_refs(), 0);
+
+    // А тут я проверяю что указатель p не поломался
+    EXPECT_EQ(p.get(), nullptr);
+    EXPECT_EQ(p.count_refs(), 0);
+}
+
+
+TEST(CopyAssigment, PointerWithValue)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    int* another_raw_p = new int(10);
+    Pointers::SharedPTR<int> p2{another_raw_p};
+    EXPECT_EQ(p2.get(), another_raw_p);
+
+    p2 = p;
+
+    EXPECT_EQ(p2.get(), raw_p);
+    EXPECT_EQ(p2.count_refs(), 2);
+
+    // А тут я проверяю что указатель p не поломался
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(p.count_refs(), 2);
+}
+
+
+TEST(CopyAssigment, CheckDeletingMemory)
+{
+    bool delete_flag = false;
+    TestClass* raw_p = new TestClass(&delete_flag);
+    Pointers::SharedPTR p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    bool another_delete_flag = false;
+    TestClass* another_raw_p = new TestClass(&another_delete_flag);
+    Pointers::SharedPTR another_p{another_raw_p};
+    EXPECT_EQ(another_p.get(), another_raw_p);
+
+    another_p = p;
+
+    EXPECT_EQ(delete_flag, false);
+    EXPECT_EQ(another_delete_flag, true);
+}
+
+
+TEST(CopyAssigment, SelfCopy)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR<int> p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+
+    p = p;  // А вдруг компилятор выкидывает эту строчку кода???
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(p.count_refs(), 1);
+}
+
+
+
+TEST(Destructor, DefaultDeleter)
+{
+    bool delete_flag = false;
+
+    {
+        TestClass* raw_p = new TestClass(&delete_flag);
+        Pointers::SharedPTR p{raw_p};
+        EXPECT_EQ(p.get(), raw_p);
+    }
+
+    EXPECT_EQ(delete_flag, true);
+}
+
+
+TEST(Destructor, CustomDeleter)
+{
+    bool deleted_obj_flag = false;
+    CustomDeleter d{&deleted_obj_flag};
+
+    {
+        int* raw_p = new int(10);
+        Pointers::SharedPTR p{raw_p, d};
+        EXPECT_EQ(p.get(), raw_p);
+    }
+
+    EXPECT_EQ(deleted_obj_flag, true);
+}
+
+
+TEST(Dereference, ByAsterisk)
+{
+    Pointers::SharedPTR p{new int(10)};
+    EXPECT_EQ(*p, 10);
+}
+
+
+TEST(Dereference, ByArrow)
+{
+    std::vector<int>* v = new std::vector<int>();
+    v->push_back(1);
+    v->push_back(2);
+    v->push_back(3);
+    Pointers::SharedPTR p{v};
+    EXPECT_EQ(p->size(), 3);
+}
+
+
+TEST(GettingPointer, GettingPointer)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR p{raw_p};
+    EXPECT_EQ(p.get(), raw_p);
+}
+
+
+TEST(OperatorBool, EmptyPointer)
+{
+    Pointers::SharedPTR<float> p{};
+    EXPECT_EQ(p, false);
+}
+
+
+TEST(OperatorBool, PointerWithValue)
+{
+    Pointers::SharedPTR p{new float(5.0f)};
+    EXPECT_EQ(p, true);
+}
+
+
+TEST(RefCounter, RefCounter)
+{
+    Pointers::SharedPTR<int> p1{};
+    EXPECT_EQ(p1.count_refs(), 0);
+    
+    std::cout << 1 << ' ' << std::endl;
+
+    p1 = new int(5);
+    std::cout << 2 << ' ' << std::endl;
+    EXPECT_EQ(p1.count_refs(), 1);
+    std::cout << 3 << ' ' << std::endl;
+
+
+    Pointers::SharedPTR p2 = p1;
+    EXPECT_EQ(p1.count_refs(), 2);
+
+
+    {
+        Pointers::SharedPTR p3 = p1;
+        EXPECT_EQ(p1.count_refs(), 3);
+    }
+
+    std::cout << 5 << ' ' << std::endl;
+
+    EXPECT_EQ(p1.count_refs(), 2);
+
+    std::cout << 6 << ' ' << std::endl;
+
+    p2 = nullptr;
+    EXPECT_EQ(p1.count_refs(), 1);
+}
+
+
+TEST(Release, EmptyPointer)
+{
+    Pointers::SharedPTR<TestClass> p{};
+
+    EXPECT_EQ(p.get(), nullptr);
+
+    p.release();
+
+    EXPECT_EQ(p.get(), nullptr);
+}
+
+
+TEST(Release, PointerWithValue)
+{
+    bool delete_flag = false;
+    TestClass* raw_p = new TestClass(&delete_flag);
+    Pointers::SharedPTR p{raw_p};
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(delete_flag, false);
+
+    p.release();
+
+    EXPECT_EQ(p.get(), nullptr);
+    EXPECT_EQ(delete_flag, true);
+}
+
+
+TEST(Reset, WithoutParameters)
+{
+    bool delete_flag = false;
+    TestClass* raw_p = new TestClass(&delete_flag);
+    Pointers::SharedPTR p{raw_p};
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(delete_flag, false);
+
+    p.reset();
+
+    EXPECT_EQ(p.get(), nullptr);
+    EXPECT_EQ(delete_flag, true);
+}
+
+
+TEST(Reset, WithParameters)
+{
+    bool delete_flag = false;
+    TestClass* raw_p = new TestClass(&delete_flag);
+    Pointers::SharedPTR p{raw_p};
+
+    EXPECT_EQ(p.get(), raw_p);
+    EXPECT_EQ(delete_flag, false);
+
+    bool another_delete_flag = false;
+    TestClass* another_raw_p = new TestClass(&another_delete_flag);
+    p.reset(another_raw_p);
+
+    EXPECT_EQ(p.get(), another_raw_p);
+    EXPECT_EQ(delete_flag, true);
+    EXPECT_EQ(another_delete_flag, false);
+}
+
+
+TEST(Swap, Swap)
+{
+    bool delete_flag = false;
+    TestClass* raw_p = new TestClass(&delete_flag);
+    Pointers::SharedPTR p{raw_p};
+
+    bool another_delete_flag = false;
+    TestClass* another_raw_p = new TestClass(&another_delete_flag);
+    Pointers::SharedPTR another_p{another_raw_p};
+
+    p.swap(another_p);
+
+    EXPECT_EQ(p.get(), another_raw_p);
+    EXPECT_EQ(another_p.get(), raw_p);
+
+    EXPECT_EQ(delete_flag, false);
+    EXPECT_EQ(another_delete_flag, false);
+}
+
+
+// TEST(Comparing, SameRawPointers)
+// {
+//     int* raw_p = new int(10);
+//     Pointers::SharedPTR p{raw_p};
+//     Pointers::SharedPTR another_p{raw_p};
+
+//     EXPECT_NE(p, another_p);
+// }
+
+
+TEST(Comparing, CopiedPointer)
+{
+    int* raw_p = new int(10);
+    Pointers::SharedPTR p{raw_p};
+    Pointers::SharedPTR another_p{p};
+
+    EXPECT_EQ(p, another_p);
+}
+
+
+TEST(MakeShared, Empty)
+{
+    Pointers::SharedPTR<int> p = Pointers::make_shared<int>();
+    EXPECT_EQ(p.get(), nullptr);
+}
+
+
+TEST(MakeShared, WithParameters)
+{
+    Pointers::SharedPTR<Point> p = Pointers::make_shared<Point>(1, 2);
+    EXPECT_EQ(p->x(), 1);
+    EXPECT_EQ(p->y(), 2);
+}
+
+
+TEST(MakeShared, FromAnotherSharedPointer)
+{
+    int counter = 0;
+    Pointers::SharedPTR<TestClassWithCounter> p = Pointers::make_shared<TestClassWithCounter>(&counter);
+    EXPECT_EQ(counter, 1);
+
+    Pointers::SharedPTR<TestClassWithCounter> another_p = Pointers::make_shared<TestClassWithCounter>(p);
+    EXPECT_EQ(counter, 1);
+
+    EXPECT_EQ(p.count_refs(), 2);
+    EXPECT_EQ(p.get(), another_p.get());
+}
